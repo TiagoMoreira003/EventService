8d2c02b9f0f8703f403f5ec2d8729d60e7b03352
Joao Monteiro <al76117@alunos.utad.pt>
2024-04-13 00:07:51 +0100

    Revert "delete"
    This reverts commit 9ac53089c9696887f4f0a5dff123fd89ab038931.

---
+++ src/Infrastructure/EntityConfiguration/EventEntityTypeConfiguration.cs
@@ -0,0 +1,47 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="EventEntityTypeConfiguration.cs" company="KROWN">
+//     Copyright (c) KROWN. All rights reserved.
+// </copyright>
+// <summary>
+// EventEntityTypeConfiguration
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace EventService.Infrastructure.EntityConfiguration
+{
+	using EventService.Domain.AggregateModels.Event;
+	using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+	/// <summary>
+	///   <see cref="EventEntityTypeConfiguration" />
+	/// </summary>
+	/// <seealso cref="EntityTypeConfiguration{Event}"/>
+
+	internal class EventEntityTypeConfiguration : EntityTypeConfiguration<Event>
+	{
+		/// <summary>
+		/// Gets the name of the table.
+		/// </summary>
+		/// <value>
+		/// The name of the table.
+		/// </value>
+		protected override string TableName => "Event";
+
+		/// <summary>
+		/// Configures the entity.
+		/// </summary>
+		/// <param name="builder">The builder.</param>
+		protected override void ConfigureEntity(EntityTypeBuilder<Event> builder)
+		{
+			builder.HasMany(f => f.Artists);
+			builder.Property(t => t.Date)
+				.IsRequired();
+			builder.Property(t => t.MusicType)
+				.IsRequired();
+			builder.Property(t => t.Description)
+				.HasMaxLength(300)
+				.IsRequired();
+			builder.HasOne(t => t.Location);
+		}
+	}
+}
