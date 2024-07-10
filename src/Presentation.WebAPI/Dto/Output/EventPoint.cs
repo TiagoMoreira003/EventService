b5e9635d17e3b231d58f28f0b546991133348ca6
Alexandre <al76741@alunos.utad.pt>
2024-07-10 19:28:35 +0100

    KROWN-94 : Update "DeleteEvent" feature + update Dtos from read + getall
    I deleted some dtos that are duplicated, make changes in the dtos output because we need the imageId in FE and I put the delete image in the delete feature

--- src/Presentation.WebAPI/Dto/Output/EventPoint.cs
+++ src/Presentation.WebAPI/Dto/Output/EventPoint.cs
@@ -8,32 +8,41 @@
 // --------------------------------------------------------------------------------------------------------------------
 namespace EventService.Presentation.WebAPI.Dto.Output
 {
-    /// <summary>
-    /// <see cref="EventPoint" />
-    /// </summary>
-    public class EventPoint
-    {
-        /// <summary>
-        /// Gets the name.
-        /// </summary>
-        /// <value>
-        /// The name.
-        /// </value>
-        public string Name { get; init; }
-        /// <summary>
-        /// Gets the location.
-        /// </summary>
-        /// <value>
-        /// The location.
-        /// </value>
-        public LocationWAdress Location { get; init; }
+	/// <summary>
+	/// <see cref="EventPoint" />
+	/// </summary>
+	public class EventPoint
+	{
+		/// <summary>
+		/// Gets the image identifier.
+		/// </summary>
+		/// <value>
+		/// The image identifier.
+		/// </value>
+		public Guid ImageId { get; init; }
 
-        /// <summary>
-        /// Gets the uu identifier.
-        /// </summary>
-        /// <value>
-        /// The uu identifier.
-        /// </value>
-        public Guid UUId { get; init; }
-    }
+		/// <summary>
+		/// Gets the location.
+		/// </summary>
+		/// <value>
+		/// The location.
+		/// </value>
+		public LocationWAdress Location { get; init; }
+
+		/// <summary>
+		/// Gets the name.
+		/// </summary>
+		/// <value>
+		/// The name.
+		/// </value>
+		public string Name { get; init; }
+
+		/// <summary>
+		/// Gets the uu identifier.
+		/// </summary>
+		/// <value>
+		/// The uu identifier.
+		/// </value>
+		public Guid UUId { get; init; }
+	}
 }
